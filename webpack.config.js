3237331f2322774011ccf24d11989f8001d60126
heense <hrmadsen89@gmail.com>
2017-06-19 21:46:17 +0200

    Adding webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
         path: __dirname + "/dist",
         filename: './app.bundle.js'
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new HtmlWebpackPlugin({
+    title: 'Project',
+    minify: {
+        collapseWhitespace: false
+    },
+    hash: true,
+    template: './src/js/index.html', // Load a custom template (ejs by default see the FAQ for details)
+  })]
 }
